2d8d22ca2e242c97b5419b8f928b4a02098963ba
furvietto <fulvio-94@hotmail.it>
2022-02-04 11:55:10 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+include_once __DIR__ . "./db.php";
+foreach ($auto_usate as $value) {
+    var_dump($value);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
